ded5dba1a20fc178dc7d09985fb915fe66496b52
Ernesto B <ejbcode@gmail.com>
2021-03-15 11:30:35 -0300

    change the .envlocal

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,11 +8,11 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
 
-  // messagingSenderId: "1071758967789",
-  // appId: "1:1071758967789:web:66e611e0504fe2f9cf5de4",
+  messagingSenderId: "1071758967789",
+  appId: "1:1071758967789:web:66e611e0504fe2f9cf5de4",
 
-  messagingSenderId: "992643175883",
-  appId: "1:992643175883:web:e6d6066105074c8ff1524e",
+  // messagingSenderId: "992643175883",
+  // appId: "1:992643175883:web:e6d6066105074c8ff1524e",
 };
 
 function initFirebase() {
